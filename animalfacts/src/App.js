e8b1c2804aeed430a2f01efb68537b6fcae89e8e
School <School@DESKTOP-LCC8D49>
2022-08-16 01:40:08 +0200

    palate update mad skills

--- animalfacts/src/App.js
+++ animalfacts/src/App.js
@@ -1,11 +1,24 @@
 import NavBar from './Components/NavBar'
+import { createTheme, ThemeProvider} from '@mui/material/styles'
+
+//Colour palate = turquois: #40E0D0, medium aquamarine: #68E7B0, light green: #90EE90, Cedar Chest: #DB504A
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#68E7B0'
+    }
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <NavBar/>
+        <h1>Hello World</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
