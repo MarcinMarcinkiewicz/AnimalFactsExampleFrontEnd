fae5c18d43754357077a91c852fdc05fc6c5c0ba
School <School@DESKTOP-LCC8D49>
2022-08-15 21:28:50 +0200

    somewhat fix

--- animalfacts/src/Components/NavBar.js
+++ animalfacts/src/Components/NavBar.js
@@ -5,30 +5,29 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+//import MenuIcon from '@mui/icons-material/Menu';
 
 const NavBar = () => {
     return (
-        // <Box sx={{ flexGrow: 1 }}>
-        //   <AppBar position="static">
-        //     <Toolbar>
-        //       <IconButton
-        //         size="large"
-        //         edge="start"
-        //         color="inherit"
-        //         aria-label="menu"
-        //         sx={{ mr: 2 }}
-        //       >
-        //         <MenuIcon />
-        //       </IconButton>
-        //       <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        //         News
-        //       </Typography>
-        //       <Button color="inherit">Login</Button>
-        //     </Toolbar>
-        //   </AppBar>
-        // </Box>
-        <h1>Hellower world</h1>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                {/* <MenuIcon /> */}
+              </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                News
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
       );
 }
 
